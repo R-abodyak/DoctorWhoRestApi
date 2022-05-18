195fbac41af95f6b19211ee7c299d05662a6e152
R-abodyak <rawanabodiak12@gmail.com>
2022-05-18 09:24:53 +0300

    add doctor interface and base repo

---
+++ DoctorWho2/Controllers/DoctorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DoctorWho2.Controllers
+{
+    [ApiController]
+    [Route("api/doctors")]
+    public class DoctorController:ControllerBase
+    {
+
+
+    }
+}
