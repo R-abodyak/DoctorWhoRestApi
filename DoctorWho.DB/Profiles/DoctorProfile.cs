bcbd31f6c6ee0f288500206a613bea2beb6ef63e
R-abodyak <rawanabodiak12@gmail.com>
2022-05-24 02:26:55 +0300

    add mapping for doctor and get doctor

---
+++ DoctorWho.DB/Profiles/DoctorProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using DoctorWho.DB.Models;
+using DoctorWho.DB.Resources;
+
+namespace DoctorWho.DB.Profiles
+{
+    public class DoctorProfile:Profile
+    {
+        public DoctorProfile()
+        {
+            CreateMap<Doctor ,DoctorDto>();
+           
+        }
+    }
+}
