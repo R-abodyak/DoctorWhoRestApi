bba17fb2b9e1a57e0b50edb49220a3028cb0a841
R-abodyak <rawanabodiak12@gmail.com>
2022-05-25 08:33:59 +0300

    add method getall episode to repo

--- DoctorWho.DB/Repositories/EpisodeRepository.cs
+++ DoctorWho.DB/Repositories/EpisodeRepository.cs
@@ -1,6 +1,25 @@
-﻿namespace DoctorWho
+﻿using DoctorWho.DB;
+using DoctorWho.DB.Models;
+using DoctorWho.DB.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoctorWho
+{
+    public class EpisodeRepository:BaseRepository, IEpisodeRebository
     {
-    internal class EpisodeRepository
+        public EpisodeRepository(DoctorWhoCoreDbContext doctorWhoCoreDbContext)
+            : base(doctorWhoCoreDbContext)
+
         {
         }
+        public async Task<IEnumerable<Episode>> GetAllEpisodeAsync()
+        {
+            var Episodes = await _context.Episodes.ToListAsync();
+            return Episodes;
+
+        }
     }
+}
---
+++ DoctorWho2/Controllers/EpisodeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DoctorWho2.Controllers
+{
+
+    [ApiController]
+    [Route("api/doctors")]
+    public class EpisodeController:ControllerBase
+    {
+        private readonly IDoctorService _doctorService;
+        private readonly IMapper _mapper;
+
+        public DoctorController(IDoctorService doctorService ,IMapper mapper)
+        {
+            _doctorService = doctorService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<DoctorDto>>> GetDoctors()
+        {
+            var DoctorList = await _doctorService.GetAllDoctorAsync();
+
+            if( DoctorList == null )
+            {
+                return NotFound();
+            }
+
+            var DoctorDtoList = _mapper.Map<IEnumerable<DoctorDto>>(DoctorList);
+            return Ok(DoctorDtoList);
+
+        }
+
+    }
+}
