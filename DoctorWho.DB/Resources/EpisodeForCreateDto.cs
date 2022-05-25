f4d8246fc4dd278a2125433ef8a6cc2e31853c40
R-abodyak <rawanabodiak12@gmail.com>
2022-05-25 09:37:49 +0300

    add EpisodeDto for create

--- DoctorWho.DB/Resources/EpisodeForCreateDto.cs
+++ DoctorWho.DB/Resources/EpisodeForCreateDto.cs
@@ -2,29 +2,19 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace DoctorWho.DB.Models
+namespace DoctorWho.DB.Resources
 {
-    public class Episode
+    public class EpisodeForCreateDto
     {
-        public int EpisodeId { get; set; }
+
         public int SeriesNumber { get; set; }
         public int EpisodeNumber { get; set; }
         public string EpisodeType { get; set; }
         public string Tittle { get; set; }
         public DateTime EpisodeDate { get; set; }
         public string Notes { get; set; }
-
-        //FK for Doctor 
         public int DoctorId { get; set; }
         public int AuthorId { get; set; }
-
-        //references for relations
-        public Author Author { get; set; }
-        public Doctor Doctor { get; set; }
-
-        public ICollection<EpisodeCompanion> EpisodeCompanion { get; set; }
-        public ICollection<EpisodeEnemy> EpisodeEnemies { get; set; }
-
 
     }
 }
